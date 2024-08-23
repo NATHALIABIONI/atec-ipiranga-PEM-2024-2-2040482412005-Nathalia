8a2126875c655ed25821a865ea47ef1c5f234a86
NATHALIABIONI <nathalia.bioni@yahoo.com.br>
2024-08-23 20:08:28 -0300

    Update PEM -Atividade-N1-2.c

--- PEM-Atividade-N1-2/PEM -Atividade-N1-2.c
+++ PEM-Atividade-N1-2/PEM -Atividade-N1-2.c
@@ -12,29 +12,29 @@ int main() {
         "Linguagem JavaScript"
     };
     
-    // Processamento para as 5 áreas
+    
     for (i = 0; i < 5; i++) {
         printf("Digite a quantidade de notas para %s: ", areas[i]);
         scanf("%d", &N);
         
-        // Verifica se N é maior que 2
+        
         if (N <= 2) {
             printf("Quantidade de notas inválida. Deve ser maior que 2.\n");
             return 1; // Termina o programa caso N <= 2
         }
         
-        // Ler as N notas para a área
+        
         printf("Digite as %d notas para %s: ", N, areas[i]);
         for (j = 0; j < N; j++) {
             scanf("%f", &notas[j]);
         }
         
-        // Inicializar variáveis
+        
         soma = 0;
         maior = notas[0];
         menor = notas[0];
         
-        // Encontrar maior, menor e somar todas as notas
+        
         for (j = 0; j < N; j++) {
             soma += notas[j];
             if (notas[j] > maior) {
@@ -45,17 +45,17 @@ int main() {
             }
         }
         
-        // Excluir a maior e a menor nota da soma
+        
         soma = soma - maior - menor;
         
-        // Imprimir a nota da área
+        
         printf("Nota de %s = %.0f\n", areas[i], soma);
         
-        // Acumular a soma final do candidato
+        
         nota_final_candidato += soma;
     }
     
-    // Imprimir a nota final do candidato
+    
     printf("Nota final do candidato = %.0f\n", nota_final_candidato);
 
     return 0;
